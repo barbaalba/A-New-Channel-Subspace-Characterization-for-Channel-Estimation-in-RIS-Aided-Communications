10ea26f94893a3f62d3770c32758c9c2126f0cc9
barbaalba <39873583+barbaalba@users.noreply.github.com>
2022-10-14 17:26:58 +0200

    Update BuildDFT.m
    Added the reference

--- BuildDFT.m
+++ BuildDFT.m
@@ -1,4 +1,9 @@
 function o = BuildDFT(M)
+% To build a DFT precoder matrix according to 
+% T. L. Jensen and E. De Carvalho, "An Optimal Channel Estimation Scheme 
+% for Intelligent Reflecting Surfaces Based on a Minimum Variance Unbiased 
+% Estimator," ICASSP 2020 - 2020 IEEE International Conference on 
+% Acoustics, Speech and Signal Processing (ICASSP), 2020
 o = zeros(M,M);
 for t = 1:M
     for m = 1:M
