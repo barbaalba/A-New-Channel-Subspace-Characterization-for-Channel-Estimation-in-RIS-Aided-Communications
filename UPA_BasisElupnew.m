7e0fd9f50f5023d5b19d3cfae843b66bf3eddee4
Mehdino <mehdi.haghshenas93@yahoo.com>
2023-05-03 13:33:12 +0200

    Complete Solution
    Fixed the problem in \lambda/2

--- UPA_BasisElupnew.m
+++ UPA_BasisElupnew.m
@@ -16,11 +16,16 @@ L_H = M_H * d_H;
 
 %% first find the elevation orthogonal to 90 degree
 theta = []; % 0 as the elevation reference
-for k = -M_V:1:M_V
+
+for k = -2*d_V*M_V:1:2*d_V*M_V
     omega = k / L_V;
     val = asin(omega + sin(elref));
     if isreal(val)
-        theta = [theta val];
+        if (val == pi/2 || val == -pi/2) && d_V == 1/2 && ismember(-val,theta)
+        
+        else
+            theta = [theta val];
+        end
     end
 end
 
@@ -33,13 +38,16 @@ fn = fieldnames(phi);
 
 %% Build the azimuth for each elevation angle
 for j = 1:length(theta)
-
-    for k = -M_H:1:M_H
+    for k = -2*M_H*d_H:1:2*d_H*M_H % -2::2
         gama = k/L_H;
         gama = gama/cos(theta(j)) + sin(azref);
         val = asin(gama);
         if isreal(val)
-            phi.(fn{j}) = [phi.(fn{j}) val];
+            if (val == pi/2 || val == -pi/2) && d_H == 1/2 && ismember(-val,phi.(fn{j}))
+
+            else
+                phi.(fn{j}) = [phi.(fn{j}) val];
+            end
         end
     end
 end
